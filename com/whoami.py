352435cef8b43b8ac274fe5dff467403044e5612
Ashlynn <ashlynn.wonderland@gmail.com>
2016-01-12 21:19:56 -0500

    .cid

--- com/whoami.py
+++ com/whoami.py
@@ -7,11 +7,25 @@ class WhoAmI:
     async def run(self, params, message):
         await self.client.send_message(message.channel, message.author.id)
 
+
+class ChannelID:
+    def __init__(self, client):
+        self.client = client
+
+    async def run(self, params, message):
+        await self.client.send_message(message.channel, message.channel.id)
+
 commands = {
     'whoami': {
         'visible': True,
         'description': "Prints your user id",
         'example': ".whoami",
         'class': WhoAmI
+    },
+    'cid': {
+        'visible': False,
+        'description': "Prints the channel id",
+        'example': ".cid",
+        'class': ChannelID
     }
 }
