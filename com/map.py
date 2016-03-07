b366eab57a7860419e9c096cbeee9aa2653cd427
Ashlynn <ashlynn.wonderland@gmail.com>
2016-03-07 16:02:55 -0500

    satellite map

--- com/map.py
+++ com/map.py
@@ -1,14 +1,17 @@
 from urllib.parse import quote_plus
 
+
 class Map:
     def __init__(self, client):
         self.client = client
 
     async def run(self, params, message):
-        await self.client.send_message(message.channel, 'http://maps.google.com/maps/api/staticmap?zoom=10&size=400x180&markers=color:blue|{0}'.format(quote_plus(' '.join(params))))
+        url = 'http://maps.google.com/maps/api/staticmap?maptype=satellite&zoom=15&size=400x180&markers=color:blue|{0}'.format(quote_plus(' '.join(params)))
+        await self.client.send_message(message.channel, url
 
-commands = {
-        'map':{
+
+commands={
+        'map': {
             'visible': True,
             'description': "Pulls up a static map for an address or coordinates",
             'example': ".map <address>",
